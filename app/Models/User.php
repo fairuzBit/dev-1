d5e9289d748b3abc4acf53886d1ca805ef3e1ff2
fairuzBit <afzulfigd@gmail.com>
2026-04-29 21:42:10 +0700

    menambahkan scramble

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,7 +23,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'phone',
+        'role',
+        'university',
+        'prodi',
+        'nim',
     ];
 
     /**
